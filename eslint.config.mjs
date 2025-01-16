37da8151a3a1d06e7c6010e8b1eea0f221985595
malinjr07 <malinjr07@gmail.com>
2025-01-17 03:09:36 +0600

    Husky Added to Lint on Push

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,9 +11,6 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    // ignores: ['src/stories/*'],
-  },
 ];
 
 export default eslintConfig;
